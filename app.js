d2a2c3b1ade4ba1b0f54bb3b24629f80501c0348
Waleed-Asim <83092895+Waleed-Asim@users.noreply.github.com>
2021-06-05 03:16:15 +0500

    Add initial files

---
+++ app.js
@@ -0,0 +1,40 @@
+// Q1
+
+// function power(a,b){
+//     return a + b
+// }
+
+// console.log(power(2,3));
+
+// Q2
+
+// function leapYear(){
+
+// var userYear = +prompt("Enter any year");
+// if ((userYear % 4 == 0 && userYear % 100 !== 0) || userYear % 400 == 0) {
+//   alert("This is a leap year.");
+// } else {
+//   alert("Not a leap year.");
+// }
+
+// }
+
+// leapYear();
+
+
+
+// Q3
+
+// function calc() {
+//     var a = +prompt("Write a number");
+//     var b = +prompt("Write another number");
+//     return a + b;
+// }
+// console.log(calc());
+
+// Q5
+
+// function calc(a,b) {
+//     return (a + b)*2;
+// }
+// console.log(calc(5,2));
